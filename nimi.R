b688834d415b2b896d0b57e3d59b6055fcae8d05
hannatuus <hannatuus@gmail.com>
2016-06-27 14:59:37 +0300

    EL toetuste saajad ja andjad

---
+++ nimi.R
@@ -0,0 +1,17 @@
+#install.packages("ggplot2")
+library(ggplot2)
+#install.packages("devtools")
+library("devtools")
+#install_github("RRisto/riigiteenused")
+library(riigiteenused)
+#install.packages("rjson")
+library(rjson)
+x=andmedSisse("https://www.riigiteenused.ee/api/et/all")
+
+#install.packages("dplyr")
+library("dplyr")
+
+#install.packages("reshape2")
+library(reshape2)
+toetus = read.csv2("./Data/toetuse_saajad.csv")
+
